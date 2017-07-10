4fc5c95fed9f47d0fb5f0ba6c7f14c97b2618cb0
shahrul nizam <skalotta69@gmail.com>
2017-07-10 18:57:35 +0800

    Update 05.sh

--- 05.sh
+++ 05.sh
@@ -1,13 +1,13 @@
 #
 #!/bin/bash
 #Script mengunci akaun SSH/VPN
-#Doctype | Malaysian Phreaker Knowledge
+#Skalotta | Malaysian Phreaker Copy
 #
 
 read -p "Username Account going to be lock: " User
 passwd -l $User
 
 echo "==============================================="
-echo "Doctype | Malaysian Phreaker Knowledge | FrogyX"
+echo "Skalotta | Malaysian Phreaker Knowledge | KS"
 echo "==============================================="
 echo ""
