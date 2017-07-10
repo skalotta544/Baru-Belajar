6d9afbe044cbaef05e0ec610ed03beece900a3f5
shahrul nizam <skalotta69@gmail.com>
2017-07-10 18:58:09 +0800

    Update 06.sh

--- 06.sh
+++ 06.sh
@@ -8,6 +8,6 @@ read -p "Username Account going to be unlock: " User
 passwd -u $User
 
 echo "==============================================="
-echo "Doctype | Malaysian Phreaker Knowledge | FrogyX"
+echo "Skalotta | Malaysian Phreaker Knowledge | KS"
 echo "==============================================="
 echo ""
